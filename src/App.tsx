6cecaade99a3ab696bc9b3f7c602799831768f0e
Witold Nawrot <witold.naw@gmail.com>
2023-03-21 23:09:44 +0100

    add authSlice, Store - Redux Toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,30 @@ import { FoRegister } from './modules/HomeModule/pages/Form/Formmm';
 import { FormLogin } from './modules/HomeModule/pages/FormLogin/FormLogin';
 import { Login } from './modules/HomeModule/pages/FormLogin/Login';
 import { UserHome } from './modules/HomeModule/pages/UserHome';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { setUser, clearUser } from '../src/modules/HomeModule/components/Auth/AuthSlice';
+import { auth } from '../src/API/firebase';
 
-const App: React.FC = () => (
-  <>
+
+const App: React.FC = () => {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        dispatch(setUser(user));
+      } else {
+        dispatch(clearUser());
+      }
+    });
+
+    return unsubscribe;
+  }, [dispatch]);
+
+  return (
+    <>
     <Router>
         <Routes>
           <Route path="register" element={<FoRegister  />} />
@@ -16,6 +37,9 @@ const App: React.FC = () => (
       </Router>
       
   </>
-);
+  );
+}
 
 export default App;
+
+
---
+++ src/modules/HomeModule/components/Auth/Store.tsx
@@ -0,0 +1,11 @@
+// import { configureStore } from '@reduxjs/toolkit';
+// import authReducer from './AuthSlice';
+
+// const Store = configureStore({
+//   reducer: {
+//     auth: authReducer,
+//     // Dodaj tutaj inne reducery, jeśli masz
+//   },
+// });
+
+// export default Store;
